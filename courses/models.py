cf200495729e1a30d0abc8d31365c7736938b51c
ST94 <ST94.dev@gmail.com>
2017-02-04 19:32:22 -0500

    Set up database for scraping course codes

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    uw_id = models.CharField(max_length=6)
+    category = models.ForeignKey("Category")
+    course_number = models.IntegerField()
+    prerequisites = models.ManyToManyField("self", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.name
+
+
+class Major(models.Model):
+    name = models.CharField(max_length=255)
+    courses = models.ManyToManyField("Course")
+
+    def __str__(self):
+        return self.name
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=255)
+    course = models.ManyToManyField("Course")
+
+    def __str__(self):
+        return self.name
